c2c2bc71d575857305efc4be7d0160d4a676c022
tquangdo <trangia61@gmail.com>
2020-10-29 19:25:57 +0900

    20201029 19:25

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One: HELLO DOTQ !!!!!</Text>
+              <Text style={styles.sectionTitle}>Step One: HELLO DOTQ ABC!!!!!</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ https://reactnative.dev/docs/environment-setup
 (choose tab "React Native CLI Quickstart", NOT "Expo CLI Quickstart")
 
 # Run cmd:
+npm i
 npx react-native run-android
